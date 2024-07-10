8b0ae99bd1e55cb2d60a4adcf04eb4985fc7be83
Adiva <prayoga.adiva@gmail.com>
2024-07-10 19:11:38 +0800

    :sparkles: Setup Sentry.io

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,23 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://ac33ed8d97abde566f1b79a1078b3096@o4507576959565824.ingest.us.sentry.io/4507576961662976",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
